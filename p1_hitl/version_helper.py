379a6bd8f271e07380806c04aae727fbf7961f2f
William Brannon <william@pointonenav.com>
2024-12-05 23:52:22 -0800

    Raise exception in case git describe fails

--- p1_hitl/version_helper.py
+++ p1_hitl/version_helper.py
@@ -12,8 +12,16 @@ logger = logging.getLogger('point_one.hitl.runner')
 def git_describe_dut_version(args: HitlEnvArgs) -> Optional[str]:
     git = GitWrapper(args.HITL_NAUTILUS_PATH)
     try:
-        mapping = DeviceType.mapping_device_to_regex(args.HITL_BUILD_TYPE)
-        return git.describe(mapping.get(args.HITL_BUILD_TYPE, 'UNKNOWN'), args.HITL_DUT_VERSION)
+        mapping = DeviceType.mapping_device_to_regex()
+        regex = mapping.get(args.HITL_BUILD_TYPE, None)
+        if regex is None:
+            raise Exception(f'Build type {args.HITL_BUILD_TYPE} not found.')
+
+        return git.describe(regex, args.HITL_DUT_VERSION)
+
     except RuntimeError as e:
         logger.warning(f'Unable to git describe commitish {args.HITL_DUT_VERSION}: {e}')
+        return None
+    except Exception as e:
+        logger.warning(f'Failed to map build type to supported device types: {e}')
         return None
