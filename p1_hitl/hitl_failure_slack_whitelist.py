ff5b07bc4ce1947839d04b0f653026a996a17cfe
Jonathan Diamond <jonathan@pointonenav.com>
2024-12-11 20:55:27 -0800

    Add white listing.

--- p1_hitl/hitl_failure_slack_whitelist.py
+++ p1_hitl/hitl_failure_slack_whitelist.py
@@ -8,7 +8,7 @@ long and they already have tickets.
 import logging
 from typing import Any
 
-from p1_hitl.defs import HitlEnvArgs
+from p1_hitl.defs import HitlEnvArgs, TestType
 from p1_runner.device_type import DeviceType
 
 logger = logging.getLogger('point_one.hitl.failure_whitelist')
@@ -31,14 +31,34 @@ def should_failure_be_ignored(env_args: HitlEnvArgs, failure: dict[str, Any]) ->
     # Lots of known LG69T failures.
     if env_args.HITL_BUILD_TYPE.is_lg69t():
         msg_start = 'Slack ignores known LG69T failure: '
-        ignored_metrics = ['max_velocity', 'fixed_max_velocity', 'cpu_usage', 'seq_num_check']
-        if failure['name'] == 'no_error_msgs' and 'Unable to allocate ImuMeasurement' in failure['context']:
-            logger.warning(msg_start + '"Unable to allocate ImuMeasurement" event.')
-            return True
+        ignored_metrics = [
+            'max_velocity',
+            'fixed_max_velocity',
+            'cpu_usage',
+            'seq_num_check',
+            'time_between_reset_and_invalid']
+        if failure['name'] == 'no_error_msgs':
+            if 'Unable to allocate ImuMeasurement' in failure['context']:
+                logger.warning(msg_start + '"Unable to allocate ImuMeasurement" event.')
+                return True
+            elif 'Timed out waiting for Teseo cold start' in failure['context']:
+                logger.warning(msg_start + '"Timed out waiting for Teseo cold start" event.')
+                return True
+
         elif failure['name'] == 'monotonic_p1time' and float(failure['context']) < 0.5:
             logger.warning(msg_start + 'monotonic_p1time')
             return True
         elif failure['name'] in ignored_metrics:
+            logger.warning(msg_start + failure['name'])
+            return True
+    elif env_args.HITL_BUILD_TYPE is DeviceType.ATLAS and env_args.get_selected_test_type() is TestType.RESET_TESTS:
+        msg_start = 'Slack ignores known Atlas reset failure: '
+        ignored_metrics = [
+            '2d_fixed_pos_error',
+            '3d_fixed_pos_error',
+            'time_between_cold_invalid_and_valid',
+            'time_between_cold_invalid_and_fixed']
+        if failure['name'] in ignored_metrics:
             logger.warning(msg_start + failure['name'])
             return True
 
