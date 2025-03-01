545be66df91c8116ecf2d7a2efb2d89afe59c415
William Brannon <william@pointonenav.com>
2025-02-28 19:25:23 -0800

    Further progress

--- p1_test_automation/ttff_test.py
+++ p1_test_automation/ttff_test.py
@@ -4,7 +4,9 @@ import socket
 import subprocess
 import sys
 import time
+from time import gmtime, strftime
 
+import logging
 import numpy as np
 import paramiko
 
@@ -25,8 +27,13 @@ CSV_FILENAME = "/home/pointone/ttff.csv"
 
 cmd_binary_path="/home/pointone/hidusb-relay-cmd"
 rf_relay_number = 1
-power_relay_number = 2
+# Associated with powering the AJ board, NOT the Pi connected to the AJ board.
+power_relay_number = 3
 hostname = "192.168.1.134"
+
+logging.basicConfig(filename='ttff.log', level=logging.INFO, filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger('ttff_test')
 
 
 class PoseMessageReader():
@@ -34,8 +41,8 @@ class PoseMessageReader():
         self.solution_valid = True
 
     def on_pose_message(self, header: MessageHeader, pose: PoseMessage, raw_bytes: bytes):
-        self.solution_valid = pose.solution_type != SolutionType.Invalid
-        print(self.solution_valid)
+        self.solution_valid = pose.solution_type != SolutionType.Invalid and pose.solution_type != SolutionType.Integrate
+        # print("Solution valid?", self.solution_valid)
 
 def power_on_device(power_rc):
     power_rc.send_cmd(on_state=True)
@@ -57,18 +64,20 @@ def is_network_available():
         return False
 
 
-
-# def on_pose_message(header: MessageHeader, pose: PoseMessage, raw_bytes: bytes):
-#     solution_valid = pose.solution_type != SolutionType.Invalid
-
 if __name__ == "__main__":
     test_start_time = time.time()
     num_tests = 700
     times = []
+    offset = 0
     for i in range(num_tests):
         print("---------------------------------------")
+
+        logger.info(f"%s: Starting test %d" % (strftime("%a, %d %b %Y %X +0000", gmtime()), i+1))
         print("STARTING TEST", i+1)
 
+        # time.sleep(30)
+
+        offset %= 30
         rf_rc = RelayController(rf_relay_number, cmd_binary_path=cmd_binary_path)
         power_rc = RelayController(power_relay_number, cmd_binary_path=cmd_binary_path)
 
@@ -76,8 +85,8 @@ if __name__ == "__main__":
 
         if not is_network_available():
             print("Cannot ping device. Attempting to power on device.")
-            power_on_device(power_rc)
-            time.sleep(30)
+            # power_on_device(power_rc)
+            # time.sleep(30)
 
 
         # Set up SSH automation tool.
@@ -93,7 +102,7 @@ if __name__ == "__main__":
                 break
             except Exception as e:
                 print("Failed to connect to host %s: %s. Going to attempt a power cycle." % (hostname, str(e)))
-                power_cycle_device(power_rc)
+                # power_cycle_device(power_rc)
                 time.sleep(15)
 
         # Check for successful connection.
@@ -103,8 +112,9 @@ if __name__ == "__main__":
             sys.exit(1)
 
         print("SSH connection successful")
-
-        # _stdin, _stdout, _stderr = ssh_client.exec_command(f"rm -rf /home/pointone/p1_fusion_engine/cache/fusion_engine")
+        print("Removing cache files")
+        _stdin, _stdout, _stderr = ssh_client.exec_command(f"rm -rf /home/pointone/p1_fusion_engine/cache/fusion_engine")
+        exit_status = _stdout.channel.recv_exit_status()
 
         # Set up TCP connection.
         port = 30200
@@ -116,14 +126,21 @@ if __name__ == "__main__":
             sock.connect((ip_address, port))
         except Exception as e:
             print("TCP connection unsuccessful. Power cycling device and starting test over.")
-            power_cycle_device(power_rc)
+            # power_cycle_device(power_rc)
             continue
 
         print("TCP connection successful")
 
+        # Create data source.
+        data_source = SocketDataSource(sock)
+
+        encoder = FusionEngineEncoder()
+
 
         # #### 1. DISCONNECT ANTENNA ####
+        logger.info("%s: Disconnecting antenna" % strftime("%a, %d %b %Y %X +0000", gmtime()))
         rf_rc.send_cmd(on_state=True)
+        # test_startup_time = time.time()
 
         print("Antenna off")
 
@@ -133,15 +150,11 @@ if __name__ == "__main__":
         # print("second command sent")
 
         #### 2. ISSUE COLD START COMMAND ####
-        print("Issuing cold start")
+        logger.info("%s: Issuing cold start" % strftime("%a, %d %b %Y %X +0000", gmtime()))
         message = ResetRequest(reset_mask=ResetRequest.COLD_START)
 
-        # Create data source.
-        data_source = SocketDataSource(sock)
 
-        encoder = FusionEngineEncoder()
         encoded_data = encoder.encode_message(message)
-
         #### 3. WAIT FOR TESEO RECEIVER TO APPLY COLD START ####
         reader = PoseMessageReader()
         decoder = FusionEngineDecoder(max_payload_len_bytes=PoseMessage.calcsize(), warn_on_unrecognized=False, return_bytes=True)
@@ -155,112 +168,111 @@ if __name__ == "__main__":
                 decoder.on_data(data)
 
                 if not reader.solution_valid:
-                    print("Solution invalid. Cold start succesfully applied.")
+                    logger.info("%s: Solution invalid. Continuing. " % strftime("%a, %d %b %Y %X +0000", gmtime()))
                     break
 
             except Exception:
                 print("Error when reading from data source.")
                 sys.exit(1)
 
+        # time.sleep(10)
+
         sock.shutdown(socket.SHUT_RDWR)
         sock.close()
+        # Wait for receiver to apply cold start.
+        time.sleep(3)
 
         #### 4. SHUT DOWN ####
-        print("Sending shutdown command")
-
+        # Might want to stop FE service here, then wait a few seconds (3ish seconds) for it to actually shut down, then delete cache files
+        print("Stopping FE service")
+        _stdin, _stdout, _stderr = ssh_client.exec_command(f"sudo systemctl stop p1_fusion_engine.service")
+        # Wait for exit status to ensure that command finished executing.
+        exit_status = _stdout.channel.recv_exit_status()
+        time.sleep(5)
+        # Remove cache files.
+        print("Removing cache files")
         _stdin, _stdout, _stderr = ssh_client.exec_command(f"rm -rf /home/pointone/p1_fusion_engine/cache/fusion_engine")
-        _stdin, _stdout, _stderr = ssh_client.exec_command(f"systemctl poweroff -i")
-        # Wait for exit status to ensure that tar command finished executing.
+        exit_status = _stdout.channel.recv_exit_status()
+        # for lnie in _stdout.readlines()
+
+
+        logger.info("%s: Sending power OFF command to device." % strftime("%a, %d %b %Y %X +0000", gmtime()))
+        power_rc.send_cmd(on_state=False)
+
+        # _stdin, _stdout, _stderr = ssh_client.exec_command(f"systemctl poweroff -i")
+
+        # Wait for exit status to ensure that command finished executing.
         # exit_status = _stdout.channel.recv_exit_status()
 
 
-
         #### 5. WAIT FOR DISCHARGE ####
-        time_to_wait_after_poweroff = 15
+        time_to_wait_after_poweroff = 10
         print("Waiting for %s seconds after power off" % time_to_wait_after_poweroff)
         time.sleep(time_to_wait_after_poweroff)
 
+
         #### 6. RECONNECT ANTENNA ####
+        logger.info("%s: Reconnecting antenna" % strftime("%a, %d %b %Y %X +0000", gmtime()))
         rf_rc.send_cmd(on_state=False)
+        # TRY THIS
+        # start_time = time.time()
         print("Antenna on")
 
         #### 7. RECONNECT POWER AND START TIMER ####
-        # # Insert sleep command in between OFF and ON command to relay to simulate power button.
-        print("Sending power on signal")
-        power_on_device(power_rc=power_rc)
+        # Insert sleep command in between OFF and ON command to relay to simulate power button.
+        print("Sending power on signal to device after offset of", offset, "seconds")
+        logger.info("%s: Sending power on signal to device after offset of %d seconds" % (strftime("%a, %d %b %Y %X +0000", gmtime()), offset))
+        time.sleep(offset)
+        offset += 1
+
+        # power_on_device(power_rc=power_rc)
+        logger.info("%s: Sending power ON command to device." % strftime("%a, %d %b %Y %X +0000", gmtime()))
+        power_rc.send_cmd(on_state=True)
         start_time = time.time()
 
-        # time.sleep(10)
+        # Wait after power on for a few seconds to turn on the p1_fusion_engine service. Found that 2 seconds
+        # is minimum that will work consistently. Otherwise, the teseo config fixup script will fail to run.
+        time.sleep(2)
 
-        #### 8. STOP TIMER AT FIRST FIX ####
-        # Continue reading until we get a pose message with a valid solution.
-        # ip_address = socket.gethostbyname(hostname)
-        # Don't attempt to connect via TCP if network is not ready.
-        # print("Checking for network availability after power on.")
-        # counter = 0
-        # success = False
-        # while time.time() - start_time < 15:
-        #     # counter += 1
-        #     # time.sleep(1)
+        # Restart fusion engine service
+        print("Starting fusion engine service")
+        _stdin, _stdout, _stderr = ssh_client.exec_command(f"sudo systemctl start p1_fusion_engine.service")
+        # Wait for exit status to ensure that command finished executing.
+        exit_status = _stdout.channel.recv_exit_status()
+        # print(output)
+        # Wait a few seconds before attempting to connect to output port. Found that 5 seconds is the minimum
+        # that will allow the connection to work consistently.
+        time_to_wait = 5
+        print("Waiting %s seconds before connecting to output port" % time_to_wait)
+        time.sleep(time_to_wait)
 
-        #     # if counter > 3:
-        #     #     break
-        #     if is_network_available():
-        #         success = True
-        #         break
-
-        #     print("Pinging network again.")
-
-        # if not success:
-        #     print("Network not found in time. Power cycling and restarting test.")
-        #     power_cycle_device(power_rc)
-        #     continue
-
-        # print("Network available after power on. Continuing.")
-
+        print("Reconnecting to output port.")
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(response_timeout_sec)
-        socket_connection_trials = 0
-        max_socket_connection_trials = 500
-        time_start_socket_connection_attempts = time.time()
-        connection_success = False
-        print("Attempting to make socket connection.")
-        while time.time() - time_start_socket_connection_attempts < 60:
-            try:
-                sock.connect((ip_address, port))
-                connection_success = True
-                break
-            except Exception:
-                socket_connection_trials += 1
-                # print("Failed to connect after power on. Going to try again")
-                # continue
-                # time_to_wait = 10
-                # print("Failed to connect. Going to attempt to power on and try again in", time_to_wait, "seconds.")
-                # time.sleep(time_to_wait)
-                # power_on_device(power_rc=power_rc)
-                # start_time = time.time()
-                # try:
-                #     sock.connect((ip_address, port))
-                # except Exception:
-                #     print("Failed to connect again. Going to power cycle and start test over.")
-                #     power_cycle_device(power_rc)
-                #     continue
-
-        if not connection_success:
-            print("Cannot make socket connection. Starting test over.")
+        try:
+            sock.connect((ip_address, port))
+        except Exception as e:
+            print("TCP connection unsuccessful. Power cycling device and starting test over.")
+            # power_cycle_device(power_rc)
             continue
 
-        print("Time spent trying to connect to socket after power on (sec):", time.time() - time_start_socket_connection_attempts)
-
+        print("TCP connection successful")
         data_source = SocketDataSource(sock)
-        print("Waiting for first valid solution.")
+
+        # Get current log ID from device.
+        stdin, stdout, stderr = ssh_client.exec_command(
+                    "echo $(basename $(ls -l /logs/current_log | awk -F'-> ' '{print $2}'))")
+        log_id = stdout.read().decode()
+        logger.info("%s: Current log ID: %s" % (strftime("%a, %d %b %Y %X +0000", gmtime()), log_id))
+
+        logger.info("%s: Waiting for first valid solution" % strftime("%a, %d %b %Y %X +0000", gmtime()))
         while True:
             try:
                 data = data_source.read(1024)
                 decoder.on_data(data)
 
                 if reader.solution_valid:
-                    print("Solution valid. System successfully navigating.")
+                    logger.info("%s: Received first valid solution. System successfully navigating." % strftime("%a, %d %b %Y %X +0000", gmtime()))
                     break
 
             except Exception:
@@ -273,6 +285,8 @@ if __name__ == "__main__":
         total_time_elapsed = end_time - start_time
         time_since_cold_start_sent = end_time - time_cold_start_sent
         times.append(total_time_elapsed)
+        logger.info("Time elapsed since power on: %d" % total_time_elapsed)
+        logger.info("Time elapsed since cold start sent: %d" % time_since_cold_start_sent)
         print("Time elapsed since power on:", total_time_elapsed)
         print("Time elapsed since cold start sent:", time_since_cold_start_sent)
         with open(CSV_FILENAME, 'a') as f:
