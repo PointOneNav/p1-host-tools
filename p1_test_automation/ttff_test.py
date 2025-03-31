1686828b40b5ef8ec26ba9b97d1657827e62bd85
William Brannon <william@pointonenav.com>
2025-03-31 10:13:53 -0700

    Progress + cleanup in TTFF test

--- p1_test_automation/ttff_test.py
+++ p1_test_automation/ttff_test.py
@@ -24,9 +24,11 @@ from p1_runner.data_source import SocketDataSource
 SSH_USERNAME = "pointone"
 SSH_KEY_PATH = "/home/pointone/.ssh/id_rsa"
 CSV_FILENAME = "/home/pointone/ttff.csv"
+NUM_TESTS = 700
 
 cmd_binary_path="/home/pointone/hidusb-relay-cmd"
 rf_relay_number = 1
+pi_relay_number = 2
 # Associated with powering the AJ board, NOT the Pi connected to the AJ board.
 power_relay_number = 3
 hostname = "192.168.1.134"
@@ -42,7 +44,6 @@ class PoseMessageReader():
 
     def on_pose_message(self, header: MessageHeader, pose: PoseMessage, raw_bytes: bytes):
         self.solution_valid = pose.solution_type != SolutionType.Invalid and pose.solution_type != SolutionType.Integrate
-        # print("Solution valid?", self.solution_valid)
 
 def power_on_device(power_rc):
     power_rc.send_cmd(on_state=True)
@@ -63,30 +64,28 @@ def is_network_available():
     except subprocess.CalledProcessError:
         return False
 
-
-if __name__ == "__main__":
+def run_tests():
     test_start_time = time.time()
-    num_tests = 700
+
     times = []
     offset = 0
-    for i in range(num_tests):
+    for i in range(NUM_TESTS):
         print("---------------------------------------")
 
         logger.info(f"%s: Starting test %d" % (strftime("%a, %d %b %Y %X +0000", gmtime()), i+1))
         print("STARTING TEST", i+1)
 
-        # time.sleep(30)
 
         offset %= 30
         rf_rc = RelayController(rf_relay_number, cmd_binary_path=cmd_binary_path)
         power_rc = RelayController(power_relay_number, cmd_binary_path=cmd_binary_path)
+        pi_rc = RelayController(power_relay_number, cmd_binary_path=cmd_binary_path)
 
-        # Make sure that fusion_engine process is running.
-
+        # Make sure that device can be reached.
         if not is_network_available():
-            print("Cannot ping device. Attempting to power on device.")
-            # power_on_device(power_rc)
-            # time.sleep(30)
+            time_to_wait = 5
+            print(f"Cannot ping device. Will try again in %d seconds." % time_to_wait)
+            time.sleep(time_to_wait)
 
 
         # Set up SSH automation tool.
@@ -96,14 +95,18 @@ if __name__ == "__main__":
         print(f'Attempting to connect to SSH service for host {hostname}')
 
         # Connect via SSH.
+        retries = 0
         while True:
             try:
                 ssh_client.connect(hostname=hostname, username=SSH_USERNAME, pkey=pkey)
                 break
             except Exception as e:
-                print("Failed to connect to host %s: %s. Going to attempt a power cycle." % (hostname, str(e)))
-                # power_cycle_device(power_rc)
+                print("Failed to connect to host %s: %s. Going to reattempt." % (hostname, str(e)))
+                if retries >= 2:
+                    print("Failed to connect. Going to power cycle device.")
+                    power_cycle_device(pi_rc)
                 time.sleep(15)
+                retries += 1
 
         # Check for successful connection.
         transport = ssh_client.get_transport()
@@ -116,7 +119,7 @@ if __name__ == "__main__":
         _stdin, _stdout, _stderr = ssh_client.exec_command(f"rm -rf /home/pointone/p1_fusion_engine/cache/fusion_engine")
         exit_status = _stdout.channel.recv_exit_status()
 
-        # Set up TCP connection.
+        # Set up TCP connection. Device under test is set up to run FE service at startup.
         port = 30200
         response_timeout_sec = 30
         ip_address = socket.gethostbyname(hostname)
@@ -126,35 +129,27 @@ if __name__ == "__main__":
             sock.connect((ip_address, port))
         except Exception as e:
             print("TCP connection unsuccessful. Power cycling device and starting test over.")
-            # power_cycle_device(power_rc)
+            power_cycle_device(pi_rc)
+            time.sleep(10)
             continue
 
         print("TCP connection successful")
 
         # Create data source.
         data_source = SocketDataSource(sock)
-
         encoder = FusionEngineEncoder()
 
-
-        # #### 1. DISCONNECT ANTENNA ####
+        ######## 1. DISCONNECT ANTENNA ########
         logger.info("%s: Disconnecting antenna" % strftime("%a, %d %b %Y %X +0000", gmtime()))
         rf_rc.send_cmd(on_state=True)
-        # test_startup_time = time.time()
 
         print("Antenna off")
 
-        # time.sleep(1)
-
-        # rc.send_cmd(on_state=False)
-        # print("second command sent")
-
-        #### 2. ISSUE COLD START COMMAND ####
+        ######## 2. ISSUE COLD START COMMAND ########
         logger.info("%s: Issuing cold start" % strftime("%a, %d %b %Y %X +0000", gmtime()))
         message = ResetRequest(reset_mask=ResetRequest.COLD_START)
-
-
         encoded_data = encoder.encode_message(message)
+
         #### 3. WAIT FOR TESEO RECEIVER TO APPLY COLD START ####
         reader = PoseMessageReader()
         decoder = FusionEngineDecoder(max_payload_len_bytes=PoseMessage.calcsize(), warn_on_unrecognized=False, return_bytes=True)
@@ -175,15 +170,13 @@ if __name__ == "__main__":
                 print("Error when reading from data source.")
                 sys.exit(1)
 
-        # time.sleep(10)
-
         sock.shutdown(socket.SHUT_RDWR)
         sock.close()
         # Wait for receiver to apply cold start.
         time.sleep(3)
 
-        #### 4. SHUT DOWN ####
-        # Might want to stop FE service here, then wait a few seconds (3ish seconds) for it to actually shut down, then delete cache files
+        ######## 4. SHUT DOWN ########
+        # Stop FE service.
         print("Stopping FE service")
         _stdin, _stdout, _stderr = ssh_client.exec_command(f"sudo systemctl stop p1_fusion_engine.service")
         # Wait for exit status to ensure that command finished executing.
@@ -193,39 +186,28 @@ if __name__ == "__main__":
         print("Removing cache files")
         _stdin, _stdout, _stderr = ssh_client.exec_command(f"rm -rf /home/pointone/p1_fusion_engine/cache/fusion_engine")
         exit_status = _stdout.channel.recv_exit_status()
-        # for lnie in _stdout.readlines()
-
 
         logger.info("%s: Sending power OFF command to device." % strftime("%a, %d %b %Y %X +0000", gmtime()))
         power_rc.send_cmd(on_state=False)
 
-        # _stdin, _stdout, _stderr = ssh_client.exec_command(f"systemctl poweroff -i")
 
-        # Wait for exit status to ensure that command finished executing.
-        # exit_status = _stdout.channel.recv_exit_status()
-
-
-        #### 5. WAIT FOR DISCHARGE ####
+        ######## 5. WAIT FOR DISCHARGE ########
         time_to_wait_after_poweroff = 10
-        print("Waiting for %s seconds after power off" % time_to_wait_after_poweroff)
+        print("Waiting for %s seconds after power off." % time_to_wait_after_poweroff)
         time.sleep(time_to_wait_after_poweroff)
 
-
-        #### 6. RECONNECT ANTENNA ####
+        ######## 6. RECONNECT ANTENNA ########
         logger.info("%s: Reconnecting antenna" % strftime("%a, %d %b %Y %X +0000", gmtime()))
         rf_rc.send_cmd(on_state=False)
-        # TRY THIS
-        # start_time = time.time()
-        print("Antenna on")
+        print("Antenna on.")
 
-        #### 7. RECONNECT POWER AND START TIMER ####
+        ######## 7. RECONNECT POWER AND START TIMER ########
         # Insert sleep command in between OFF and ON command to relay to simulate power button.
-        print("Sending power on signal to device after offset of", offset, "seconds")
+        print("Sending power on signal to device after offset of %d seconds." % offset)
         logger.info("%s: Sending power on signal to device after offset of %d seconds" % (strftime("%a, %d %b %Y %X +0000", gmtime()), offset))
         time.sleep(offset)
         offset += 1
 
-        # power_on_device(power_rc=power_rc)
         logger.info("%s: Sending power ON command to device." % strftime("%a, %d %b %Y %X +0000", gmtime()))
         power_rc.send_cmd(on_state=True)
         start_time = time.time()
@@ -234,16 +216,16 @@ if __name__ == "__main__":
         # is minimum that will work consistently. Otherwise, the teseo config fixup script will fail to run.
         time.sleep(2)
 
-        # Restart fusion engine service
-        print("Starting fusion engine service")
+        # Restart fusion engine service.
+        print("Starting Fusion Engine service.")
         _stdin, _stdout, _stderr = ssh_client.exec_command(f"sudo systemctl start p1_fusion_engine.service")
         # Wait for exit status to ensure that command finished executing.
         exit_status = _stdout.channel.recv_exit_status()
-        # print(output)
+
         # Wait a few seconds before attempting to connect to output port. Found that 5 seconds is the minimum
         # that will allow the connection to work consistently.
         time_to_wait = 5
-        print("Waiting %s seconds before connecting to output port" % time_to_wait)
+        print("Waiting %s seconds before connecting to output port." % time_to_wait)
         time.sleep(time_to_wait)
 
         print("Reconnecting to output port.")
@@ -265,6 +247,7 @@ if __name__ == "__main__":
         log_id = stdout.read().decode()
         logger.info("%s: Current log ID: %s" % (strftime("%a, %d %b %Y %X +0000", gmtime()), log_id))
 
+        ######## 8. START TIMER AT FIRST FIX ########
         logger.info("%s: Waiting for first valid solution" % strftime("%a, %d %b %Y %X +0000", gmtime()))
         while True:
             try:
@@ -281,7 +264,7 @@ if __name__ == "__main__":
 
         end_time = time.time()
 
-        #### 9. RECORD RESULTS ####
+        ######## 9. RECORD RESULTS ########
         total_time_elapsed = end_time - start_time
         time_since_cold_start_sent = end_time - time_cold_start_sent
         times.append(total_time_elapsed)
@@ -300,4 +283,8 @@ if __name__ == "__main__":
     print("Max time:", max(times), "seconds")
     print("Min time:", min(times), "seconds")
 
-    print("Time to run", num_tests, "tests:", time.time() - test_start_time, "seconds")
+    print("Time to run", NUM_TESTS, "tests:", time.time() - test_start_time, "seconds")
+
+
+if __name__ == "__main__":
+    run_tests()
