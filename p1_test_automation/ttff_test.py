c94f0a6c002f6c23e9f436fdbc36fd4bb8c43246
William Brannon <william@pointonenav.com>
2025-04-02 14:35:53 -0700

    Changes for review

--- p1_test_automation/ttff_test.py
+++ p1_test_automation/ttff_test.py
@@ -21,6 +21,14 @@ sys.path.append(repo_root)
 
 from p1_runner.data_source import SocketDataSource
 
+'''
+This script is for TTFF testing, and the underlying process and hardware setup is described in this document:
+https://docs.google.com/document/d/1kaCncm1S-A__UzS-jhh6DKekcIpC4QQg/edit?usp=sharing&ouid=116244641276191759860&rtpof=true&sd=true
+
+The relevant fusion_engine_parameters.sh file used for this testing can be found here:
+https://drive.google.com/file/d/12vRCAVdmoqSphmf5QdZk_nNNnPrl-W-_/view?usp=drive_link
+'''
+
 SSH_USERNAME = "pointone"
 SSH_KEY_PATH = "/home/pointone/.ssh/id_rsa"
 CSV_FILENAME = "/home/pointone/ttff.csv"
@@ -50,12 +58,12 @@ def power_on_device(power_rc):
     time.sleep(1)
     power_rc.send_cmd(on_state=False)
 
-def power_cycle_device(power_rc):
-    power_rc.send_cmd(on_state=True)
+def power_cycle_device(pi_rc):
+    pi_rc.send_cmd(on_state=True)
     time.sleep(10)
-    power_rc.send_cmd(on_state=False)
+    pi_rc.send_cmd(on_state=False)
     time.sleep(1)
-    power_on_device(power_rc)
+    power_on_device(pi_rc)
 
 def is_network_available():
     try:
@@ -95,6 +103,10 @@ def run_tests():
         print(f'Attempting to connect to SSH service for host {hostname}')
 
         # Connect via SSH.
+        # NOTE: The following code block involves logic that was more relevant to the previous
+        # TTFF setup, which involved shutting off and turning back on the whole DUT, rather than
+        # just the receiver. The `except` statement below hasn't found to be called in the
+        # current version of this script.
         retries = 0
         while True:
             try:
@@ -115,9 +127,6 @@ def run_tests():
             sys.exit(1)
 
         print("SSH connection successful")
-        print("Removing cache files")
-        _stdin, _stdout, _stderr = ssh_client.exec_command(f"rm -rf /home/pointone/p1_fusion_engine/cache/fusion_engine")
-        exit_status = _stdout.channel.recv_exit_status()
 
         # Set up TCP connection. Device under test is set up to run FE service at startup.
         port = 30200
@@ -162,6 +171,8 @@ def run_tests():
                 data = data_source.read(1024)
                 decoder.on_data(data)
 
+                # NOTE: currenty, we don't check for the corner case where the solution type was _already_ invalid
+                # before the cold start.
                 if not reader.solution_valid:
                     logger.info("%s: Solution invalid. Continuing. " % strftime("%a, %d %b %Y %X +0000", gmtime()))
                     break
@@ -172,7 +183,7 @@ def run_tests():
 
         sock.shutdown(socket.SHUT_RDWR)
         sock.close()
-        # Wait for receiver to apply cold start.
+        # Wait for receiver to apply cold start; a wait of 3 seconds was found to be a suffient.
         time.sleep(3)
 
         ######## 4. SHUT DOWN ########
@@ -181,13 +192,15 @@ def run_tests():
         _stdin, _stdout, _stderr = ssh_client.exec_command(f"sudo systemctl stop p1_fusion_engine.service")
         # Wait for exit status to ensure that command finished executing.
         exit_status = _stdout.channel.recv_exit_status()
+        # NOTE: In the future, we'd ideally use a `kill -9` command to ensure that the process is stopped. Further,
+        # we should check these exit status codes.
         time.sleep(5)
         # Remove cache files.
         print("Removing cache files")
         _stdin, _stdout, _stderr = ssh_client.exec_command(f"rm -rf /home/pointone/p1_fusion_engine/cache/fusion_engine")
         exit_status = _stdout.channel.recv_exit_status()
 
-        logger.info("%s: Sending power OFF command to device." % strftime("%a, %d %b %Y %X +0000", gmtime()))
+        logger.info("%s: Powering off reciever board." % strftime("%a, %d %b %Y %X +0000", gmtime()))
         power_rc.send_cmd(on_state=False)
 
 
@@ -214,6 +227,9 @@ def run_tests():
 
         # Wait after power on for a few seconds to turn on the p1_fusion_engine service. Found that 2 seconds
         # is minimum that will work consistently. Otherwise, the teseo config fixup script will fail to run.
+        #
+        # NOTE: in the future, we should modify this script so that it doesn't run the teseo config fixup
+        # script, as the fixup script may have unintended impacts on the Teseo's startup.
         time.sleep(2)
 
         # Restart fusion engine service.
@@ -235,7 +251,7 @@ def run_tests():
             sock.connect((ip_address, port))
         except Exception as e:
             print("TCP connection unsuccessful. Power cycling device and starting test over.")
-            # power_cycle_device(power_rc)
+            power_cycle_device(pi_rc)
             continue
 
         print("TCP connection successful")
@@ -247,7 +263,7 @@ def run_tests():
         log_id = stdout.read().decode()
         logger.info("%s: Current log ID: %s" % (strftime("%a, %d %b %Y %X +0000", gmtime()), log_id))
 
-        ######## 8. START TIMER AT FIRST FIX ########
+        ######## 8. STOP TIMER AT FIRST FIX ########
         logger.info("%s: Waiting for first valid solution" % strftime("%a, %d %b %Y %X +0000", gmtime()))
         while True:
             try:
