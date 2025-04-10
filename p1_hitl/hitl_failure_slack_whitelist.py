70ee8a611bd09e9ff15d9c3458e7d52a8fde39b3
Jonathan Diamond <jonathan@pointonenav.com>
2025-04-10 12:10:50 -0700

    Remove no longer relevant BMW whitelists.

--- p1_hitl/hitl_failure_slack_whitelist.py
+++ p1_hitl/hitl_failure_slack_whitelist.py
@@ -71,12 +71,6 @@ def should_failure_be_ignored(env_args: HitlEnvArgs, failure: dict[str, Any]) ->
             'fixed_max_velocity',
             'gps_time_valid',
         ]
-    elif env_args.HITL_BUILD_TYPE is DeviceType.BMW_MOTO_MIC:
-        ignore_failure = failure['name'] in [
-            'imu_msg_period',
-            'tcp1_calibration_status_rate',
-            'tcp2_p1calstatus_rate',
-        ]
 
     if ignore_failure:
         logger.warning('Slack ignores whitelisted failure: ' + failure['name'])
