7c80ddf14b9dbb26534eff0f4ea880a9d694124f
Jonathan Diamond <jonathan@pointonenav.com>
2025-04-17 12:07:39 -0700

    Fix path.

--- p1_hitl/device_interfaces/st_heading_interfaces.py
+++ p1_hitl/device_interfaces/st_heading_interfaces.py
@@ -4,4 +4,4 @@ from .big_engine_interfaces import HitlBigEngineInterface
 
 class HitlTeseoHeadingInterface(HitlBigEngineInterface):
     POLARIS_API_KEY = os.getenv('HITL_POLARIS_API_KEY')
-    RUNNER_CMD = f'POLARIS_API_KEY={POLARIS_API_KEY} ./run_teseo_heading.sh'
+    RUNNER_CMD = f'POLARIS_API_KEY={POLARIS_API_KEY} ./p1_fusion_engine/run_teseo_heading.sh'
