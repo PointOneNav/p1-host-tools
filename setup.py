57d887bee92b688a6653642868e50a959b92aafd
Adam Shapiro <adam@pointonenav.com>
2025-05-27 15:24:22 -0400

    Updated the library version number to 0.31.1.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ if sys.version_info >= (3, 7):
 
 setup(
     name='p1-host-tools',
-    version='0.31.0',
+    version='0.31.1',
     packages=find_packages(where='.'),
     install_requires=list(all_requirements),
     extras_require={
